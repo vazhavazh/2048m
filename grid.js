8bf819016551d1a9dc3e9f9f91fd95ce8725ac93
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-07-09 01:10:45 +0300

    keyboard

--- grid.js
+++ grid.js
@@ -11,11 +11,29 @@ export class Grid {
                 new Cell(gridElement, i % GRID_SIZE, Math.floor(i / GRID_SIZE))
             )
         }
+        this.cellsGroupedByColumn = this.groupCellsByColumn();
+        this.cellsGroupedByReversedColumn = this.cellsGroupedByColumn.map(column => [...column].reverse());
+        this.cellsGroupedByRow = this.groupCellsByRow();
+        this.cellsGroupedByReversedRow = this.cellsGroupedByRow.map(row => [...row].reverse());
     }
     getRandomEmptyCell() {
         const emptyCells = this.cells.filter(cell => cell.isEmpty());
-        
+
         const randomIndex = Math.floor(Math.random() * emptyCells.length);
         return emptyCells[randomIndex]
+    }
+    groupCellsByColumn() {
+        return this.cells.reduce((groupedCells, cell) => {
+            groupedCells[cell.x] = groupedCells[cell.x] || [];
+            groupedCells[cell.x][cell.y] = cell
+            return groupedCells;
+        }, [])
+    }
+    groupCellsByRow() {
+        return this.cells.reduce((groupedCells, cell) => {
+            groupedCells[cell.y] = groupedCells[cell.y] || [];
+            groupedCells[cell.y][cell.x] = cell
+            return groupedCells;
+        }, [])
     }
 }
--- tile.js
+++ tile.js
@@ -2,7 +2,7 @@ export class Tile {
     constructor(gridElement) {
         this.tileElement = document.createElement("div");
         this.tileElement.classList.add("tile");
-        this.value = Math.random() > 0.5 ? 2 : 4;
+        this.setValue(Math.random() > 0.5 ? 2 : 4);
         this.tileElement.textContent = this.value;
         gridElement.append(this.tileElement);
     }
@@ -12,4 +12,15 @@ export class Tile {
         this.tileElement.style.setProperty("--x", x);
         this.tileElement.style.setProperty("--y", y);
     }
+    setValue(value) {
+        this.value = value;
+        this.tileElement.textContent = value;
+        const bgLightness = 100 - Math.log2(value) * 9;
+        this.tileElement.style.setProperty("--bg-lightness", `${bgLightness}%`);
+        this.tileElement.style.setProperty("--text-lightness", `${bgLightness < 50 ? 90 : 10}%`);
+    }
+    removeFromDOM() {
+        this.tileElement.remove();
+    }
 }
+
